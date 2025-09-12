4590a85ccdc775ba129e96678c4ad454b8e9105f
Firoz Abbas <firozabbas.edunext@gmail.com>
2025-09-12 07:22:06 +0530

    Firoz Abbas

---
+++ src/main/java/com/spring/ai/firstproject/model/tuitor/Tut.java
@@ -0,0 +1,14 @@
+package com.spring.ai.firstproject.model.tuitor;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Tut {
+    private String title;
+    private String content;
+    private String academicYear;
+}
